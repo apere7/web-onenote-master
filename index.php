6dcc85b4c727718e84063a41743b32963c6777e4
Alejandro Pereira <alejo24174@gmail.com>
2022-08-03 11:05:29 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<link rel="stylesheet" type="text/css" href="css/estilos1.css">
+	<head>
+		<meta charset="utf-8">
+		<style type="text/css">
+				.body {
+			background-color: #8fc0a9;
+			}
+		</style>        
+
+	
+	</head>
+
+	<frameset rows="130,60,*"> 
+	<frame src="cabecera.html" name="cabecera">
+	<frame src="tema_vacio.html" name="temas">
+		<frameset cols="158,*">
+			<frame src="menu_vacio.html" name="menu">
+			<frame src="contenido_vacio.html" name="contenido">
+		</frameset> 
+	</frameset>
+
+
+</html>
